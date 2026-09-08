50c9001fee8623604512554bcc5a87b79dfa3d0f
GianBenav <b2wmywvsyq@privaterelay.appleid.com>
2026-09-07 20:47:04 -0400

    Crear Recursos.h

---
+++ include/Recursos.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class Recursos
+{
+public:
+
+    int madera = 0;
+    int piedra = 0;
+    int hierro = 0;
+    int carbon = 0;
+
+    int oro = 0;
+    int plata = 0;
+
+    int comida = 0;
+    int trigo = 0;
+
+    int cuero = 0;
+    int arcilla = 0;
+
+    void mostrarRecursos() const;
+};
